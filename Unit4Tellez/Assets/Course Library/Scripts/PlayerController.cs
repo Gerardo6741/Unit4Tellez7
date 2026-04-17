20632c5aff3370a4263da24ffb760cd667cf1177
Gerardo6741 <759369@student.dusd.net>
2026-04-17 15:17:33 -0700

    zz
    zzz

--- Unit4Tellez/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit4Tellez/Assets/Course Library/Scripts/PlayerController.cs
@@ -16,6 +16,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward  * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+    public bool hasPowerup;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
